004e9643e73d6ccf33a4fe0c0558165476c444e3
Sammy Griffiths <contact@sammygriffiths.co.uk>
2016-12-08 23:02:07 +0000

    Don't search both with and without spaces because it was breaking things

--- app/helpers/search.php
+++ app/helpers/search.php
@@ -14,7 +14,7 @@ class Search {
 
         $result = $client->request('GET', 'https://api.spotify.com/v1/search', [
             'query' => [
-                'q'    => '"'.$searchTerm.'" OR "'.str_replace(' ', '', $searchTerm).'"',
+                'q'    => $searchTerm,
                 'type' => $types
             ]
         ]);

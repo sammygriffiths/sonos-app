e2b994b817bac08d3b5ca388e5065d892c1966c7
Sammy Griffiths <contact@sammygriffiths.co.uk>
2017-04-30 16:19:35 +0100

    Add fallback for no results found

--- app/controllers/searchController.php
+++ app/controllers/searchController.php
@@ -10,13 +10,14 @@ class SearchController extends CoreController
     public function results() {
 
         $searchTerm = $this->request->query->get('term');
-        $searchTerm = $this->model->formatSearchTerm($searchTerm);
+        $searchTermFormatted = $this->model->formatSearchTerm($searchTerm);
 
-        $spotifyResults = Search::spotify($searchTerm, ['track', 'artist']);
+        $spotifyResults = Search::spotify($searchTermFormatted, ['track', 'artist']);
 
         return $this->app['twig']->render('searchResults.html.twig', [
-            'artists' => $spotifyResults->artists->items,
-            'tracks'  => $spotifyResults->tracks->items
+            'artists'    => $spotifyResults->artists->items,
+            'tracks'     => $spotifyResults->tracks->items,
+            'searchTerm' => $searchTerm
         ]);
     }
 
